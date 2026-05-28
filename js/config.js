349abbf9e5bc291852663fa31aa9125164a2c8f2
Fluent Immersion <158618477+teachermarcelo@users.noreply.github.com>
2026-05-28 15:34:49 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 const SUPABASE_URL = 'https://rvgcniaowzmsudzliozf.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2Z2NuaWFvd3ptc3Vkemxpb3pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MjQxNzQsImV4cCI6MjA5MTQwMDE3NH0.uwwKFLuK-XyPoXPrB6_CseRTiD9d-iyMQSPWrFw-l-I';
 
-// Inicializa o cliente e expõe globalmente para evitar conflitos de escopo
+// Inicializa o cliente e expõe globalmente para ser usado no app.js
 window.bncSupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 console.log('✅ Configuração do Supabase carregada.');
